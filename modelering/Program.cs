4deb880e461df18247c8efd50403c5d9a9255de2
abbepkfr <albin.ostman2@elev.ga.ntig.se>
2020-10-01 16:21:15 +0200

    oiu

---
+++ modelering/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace modelering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            jalla moPed = new jalla();
+
+            moPed.x = 40;
+
+            moPed.y = 20;
+
+            moPed.z = 5;
+
+            System.Console.WriteLine(moPed.y);
+
+            System.Console.WriteLine(moPed.x - moPed.z);
+
+
+
+
+            Console.WriteLine("Hello World");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ modelering/jalla.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace modelering
+{
+public class jalla
+{
+    public float x;
+    public float y;
+    public float z;
+}
+
+
+}
